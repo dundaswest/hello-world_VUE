00393a213595ff6b597e9179c771f650e9b2056c
Eunbyul Cho <hello@eunbyulcho.me>
2019-05-07 19:59:36 +0900

    import from basic.vue

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ["@vue/app"]
+  presets: ["@vue/app"],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: []
 };
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
+
+})
 <template>
   <div id="app">
-
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
